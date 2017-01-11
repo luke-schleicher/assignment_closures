408fa6d18646b05ccc420d077864239d0c1f9aac
luke-schleicher <luke.j.schleicher@gmail.com>
2017-01-11 11:33:05 -0600

    finished the 2nd exercise

--- closures.js
+++ closures.js
@@ -16,10 +16,12 @@ assignments.one = function(){
     // If we didn't define i in *this* function, then we would use
     //   the outermost function's i, which is 4 after iteration is
     //   finished.
+
     $(buttons[i]).on('click', function() {
       $('#clicked-btn').text('You clicked button #' + i);
     });
-  }
+
+  };
 
   for (var i = 0; i < buttons.length; i++) {
     // Place the event listener in a function called buttonClick.
@@ -29,7 +31,7 @@ assignments.one = function(){
   }
 
 
-}
+};
 
 
 
@@ -39,32 +41,32 @@ ASSIGNMENT TWO: CHEER UP THE SAD VIKING VIA CLOSURE
 
 assignments.two = function(){
 
-  var viking = {  mood: undefined,
-                  cheerUp: ( function() {
-                          //This part works!
-                          //Otherwise, it would be undefined
-                          console.log('sad');
-                          this.mood = "sad.";
-                          $('#mood').text(this.mood);
+  var viking = {  
 
-                          //So what goes wrong here?
-                          setTimeout( (function() {
-                            this.mood = "Happy!";
+    mood: undefined,
 
-                            //THIS even runs correctly!
-                            //What is UP with this? :(
-                            console.log("Cheered Up!")
-                          }), 1000);
-                      })
-           };
+    cheerUp: ( function() {
 
+      console.log('sad');
+      this.mood = "sad";
+      $('#mood').text(this.mood);
 
+      // Within the setTimeout function, 'this' referred to the window
+      // Since setTimeout is called on the window.
+      // We called the bind function on our anonymous function
+      // And set 'this' within the timeout equal to the viking object instance
+      // Rather than the window
+      setTimeout( (function() {
+        this.mood = "Happy!";
+        console.log("Cheered Up!");
+      }).bind(this), 1000);
+
+    })
+
+  };
 
   viking.cheerUp();
 
-  //waits an extra millisecond to make sure
-  //that the other setTimeout has run.
-  //The problem is NOT here
   setTimeout( function() {
     $('#mood').text(viking.mood);
   }, 1001);
